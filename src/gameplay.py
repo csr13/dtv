98178ed4fc88a37cb12ea10d90d7ed16474151d3
pxyg13 <carsaroca.13@gmail.com>
2020-03-28 00:08:12 -0600

    addedcomments for readability

--- src/gameplay.py
+++ src/gameplay.py
@@ -25,31 +25,29 @@ from characters import *
 from config import SCREENRECT
 from utils import load_image, Img, writer
 
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# Inits
+# ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+#
+# Initial setup
+#
+# ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
 pygame.init()
 clock = pygame.time.Clock()
 pygame.key.set_repeat(10, 100)
-
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# Pantalla
-
 screen = pygame.display.set_mode(SCREENRECT.size)
 pygame.display.set_caption(" Slip the Virus ")
 
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# Background
-
 Img.background = load_image("background.gif")
-
 background = pygame.Surface(SCREENRECT.size)
 for x in range(0, SCREENRECT.width, Img.background.get_width()):
     background.blit(Img.background, (x, 0))
 background_rect = background.get_rect()
 
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+#
 # The key master, holder.
+#
+# ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
 holder = Holder(
     unicorn=Unicorn("unicorn.png", spawn_location={"midleft": (50, 640 // 2)}),
@@ -57,19 +55,42 @@ holder = Holder(
     stats=StatsBar(),
 )
 
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+#
 # Main Loop
+#
+# ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
 
 while holder.unicorn.alive == bool(1):
+    
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+    #
+    # Check to see if the rest of the loop will execute, this happens because
+    # we are checking to see if the death scene has a;ready happend.
+    # here we cset the final pointage, we can that to be the first otherwise we
+    # can leak points, the we send the kill signal to the main loop.
+    #
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
-    # Check to see if the rest of the loop will execute
     if holder.unicorn.dead_scene_time <= 0:
+        holder.stats.final_points = holder.stats.get_current_game_time()
         holder.unicorn.alive = False
 
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+    #
     # Handle the background first
+    #
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
     holder.dirtyrects.append(screen.blit(background, (0, 0)))
 
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+    #
     # Evaluate vents & keys pressed
+    #
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
     for event in pygame.event.get():
         pygame.event.pump()
         key = pygame.key.get_pressed()
@@ -81,29 +102,116 @@ while holder.unicorn.alive == bool(1):
         if event.type == pygame.QUIT:
             exit(1)
 
-    # Detect collisions
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+    #
+    # Here, within this if block we index the virus that collided with
+    # the user object, the Unicorn.
+    #
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
     x_x = holder.unicorn.rect.collidelist(holder.virus_holder)
 
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+    #
+    # If a collision was found, then execute the following
+    #
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+    
     if x_x != -1:
 
+        # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+        #
+        # If the virus holder is dead then pass, because the virus was already dead
+        # nothing more to do here.
+        #
+        # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+        
         if holder.virus_holder[x_x].dead:
             pass
 
+        # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+        #
+        # If the unicorn had its shield activated when it collided with the virus
+        #
+        #   1) Signal the virus that it needs to be removed
+        #   2) Increment the viruses eliminated count by one.
+        #
+        # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
         elif holder.unicorn.shield == True:
             holder.virus_holder[x_x].dead = True
-            print("Nos chingamos a el virus ->", holder.virus_holder[x_x])
+            holder.stats.viruses_eliminated += 1
+
+        # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+        #
+        # If the unicorn did not have its shield on when the collision happened
+        # a couple of things will happen.
+        #
+        # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+        #
+        #   1) If the unicorn has no health.
+        #
+        #       1.1) Set the dead status of the unicorn to True,
+        #            This does a couple of things, first it sends a signal to the
+        #            unicorn.update() (called later) method which indicates that
+        #            the unicorn death scene count needs to decrement by one.
+        #
+        #       1.2) Initiate te unicorn death scene, which relays on a a count
+        #            to orchestrate it's duration.
+        #
+        # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+        #
+        #   2) If the unicorn has health, then a couple of things will happen.
+        #       
+        #       2.1) Send a signal to the unicorn instance that a hit occured.
+        #            This signal decrements the instance health by 5 points.
+        #
+        # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
         elif holder.unicorn.shield == False:
+
             if not holder.unicorn.health_ok():
                 holder.unicorn.dead = True
                 holder.unicorn.death_scene()
+
             else:
                 holder.unicorn.hit = True
 
-        elif holder.virus_holder[x_x].dead == False:
-            pass
-
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+    #
     # Update Viruses
+    #
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+    # 
+    # First we roll some odds for if a new virus will appear.
+    #
+    #   1) append a new virus instance to the virus holder list.
+    #
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+    #
+    # First loop is important, we update all the viruses, this does a couple of 
+    # things.
+    #
+    #   1) It checks the boundaries of the virus y axis with respect to the screen
+    #   2) It checks for a signal to see if the virus is dead and we need to
+    #      initiate a decremental count for the death scene duriation.
+    #   3) It increments the virus instance x coordinate, for movement.
+    #   4) It checks to see if there are any viruses off the screen, and pops them
+    #      it also directs the virus.
+    #
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+    #
+    # Second loop draws all the updated batch of viruses, using it draw method
+    # 
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+    #
+    # Third loop checks does a couple of things
+    #   1) It checks to see if the virus is dead, and if its time scene count
+    #      has finished, and if True then;
+    #       1.1) It calls its own method to erase the instance from the screen.
+    #
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
     if not int(random.random() * 12):
         holder.virus_holder.append(Virus("virus.gif"))
 
@@ -117,13 +225,46 @@ while holder.unicorn.alive == bool(1):
         if host.dead and host.death_scene_time < 0:
             host.erase(screen, background, holder)
 
-    # Update Unicorn
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+    #
+    # Update Unicorn & draw Unicorn
+    #
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
     holder.unicorn.update(screen, holder)
     holder.unicorn.draw(screen, holder)
-    # Update StatsBar
+
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+    #
+    # Update the stats
+    #
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
     holder.stats.update(screen, holder)
 
-    # Update display
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+    #
+    # Update display by passing the list of dirty rects collecred by each of the
+    # draw and earase methods of the instances that where manipulated.
+    # Set frames pre second with clock.tick, by default, nice.
+    # 
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
     pygame.display.update(holder.dirtyrects)
     holder.reset_dirtyrects()
     clock.tick(69)
+
+print(
+f"""
++ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + 
+
+Game stats (*_*)
+
++ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + 
+
+Total time lasted        => {holder.stats.final_points}
+Total viruses eliminated => {holder.stats.viruses_eliminated}
+
++ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + 
+"""
+)
