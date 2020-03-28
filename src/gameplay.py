c95416e0f60efbb03456165198d9ac3440cd4901
pxyg13 <carsaroca.13@gmail.com>
2020-03-28 00:14:57 -0600

    added comments for readability

--- src/gameplay.py
+++ src/gameplay.py
@@ -156,7 +156,7 @@ while holder.unicorn.alive == bool(1):
         #            unicorn.update() (called later) method which indicates that
         #            the unicorn death scene count needs to decrement by one.
         #
-        #       1.2) Initiate te unicorn death scene.
+        #       1.2) Initiate the unicorn death scene.
         #
         # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
         #
