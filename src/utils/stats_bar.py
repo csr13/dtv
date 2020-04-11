d6b4dcb2f3abe9167b8bb01d353176e693bc367e
pxyg13 <carsaroca.13@gmail.com>
2020-04-10 23:43:58 -0500

    colors based on pointage levels; stats bar background tint

--- src/utils/stats_bar.py
+++ src/utils/stats_bar.py
@@ -29,14 +29,18 @@ from utils.utils import load_image, writer
 
 
 class StatsBar(object):
-    """
-    Stats Bar class
-    """
+    """This class represents the statistics bar of the game."""
 
     def __init__(self):
         self.game_start = time.time()
         self.writer = writer
         self.dead_viruses = []
+        self.colors = {
+            "red": (255, 0, 0),
+            "green": (0, 255, 0),
+            "blue": (0, 0, 255),
+            "yellow": (255, 255, 0)
+        }
 
     def get_current_game_time(self):
         """
@@ -56,18 +60,27 @@ class StatsBar(object):
             phrase="Time",
             font="ubuntumono",
             size=12,
-            color=(255, 255, 51),
+            color=(0, 255, 0),
             where={"top": 45, "right": 360},
         )
         time_number_text, time_number_position = self.writer(
             phrase=f"{self.get_current_game_time()} ",
             font="ubuntumono",
             size=12,
-            color=(255, 255, 51),
+            color=(0, 255, 0),
             where={"top": 45, "right": 423},
         )
         holder.dirtyrects.append(screen.blit(time_number_text, time_number_position))
         holder.dirtyrects.append(screen.blit(time_text, time_position))
+
+    def stats_background(self, screen, holder):
+        """
+        Generates the stats bar background
+        """
+        background_rect = pygame.Rect(0, 0, 800, 71)
+        background_bckg = pygame.Surface(background_rect.size).convert_alpha()
+        background_bckg.fill((0,0,0, 150))
+        holder.dirtyrects.append(screen.blit(background_bckg, (0,0)))
 
 
     def generate_energy_bar_text(self, screen, holder):
@@ -79,7 +92,7 @@ class StatsBar(object):
             phrase=f"Shield ",
             font="ubuntumono",
             size=12,
-            color=(255, 255, 51),
+            color=(0, 255, 0),
             where={"top": 10, "right": 57},
         )
         holder.dirtyrects.append(screen.blit(energy_text, energy_position))
@@ -93,7 +106,14 @@ class StatsBar(object):
         energy = holder.unicorn.shield_energy
         energy_bar_rect = pygame.Rect(0, 0, energy, 35)
         energy_bar_bkgd = pygame.Surface(energy_bar_rect.size)
-        energy_bar_bkgd.fill((0, 255, 0))
+
+        if 150 < energy <= 250:
+            energy_bar_bkgd.fill(self.colors["green"])
+        elif 50 <= energy < 150:
+            energy_bar_bkgd.fill(self.colors["yellow"])
+        elif 0 <= energy < 50:
+            energy_bar_bkgd.fill(self.colors["red"])
+        
         holder.dirtyrects.append(screen.blit(energy_bar_bkgd, (70, 0)))
 
 
@@ -106,26 +126,34 @@ class StatsBar(object):
             phrase=f"Energy ",
             font="ubuntumono",
             size=12,
-            color=(255, 255, 255),
+            color=(0, 255, 0),
             where={"top": 45, "right": 57},
         )
         holder.dirtyrects.append(screen.blit(lives_text, lives_text_position))
 
 
-    def generate_unicorn_life_bar(self, screen, holder):
+    def generate_player_life_bar(self, screen, holder):
         """
-        Generate the unicorns life bar.
+        Generate the player life bar.
         """
 
         life = math.ceil(holder.unicorn.life)
-        if life < 1:
-            life = 1
+        life = 1 if life < 1 else life
+        
         try:
             life_bar_rect = pygame.Rect(0, 0, life, 33)
         except:
             return
+
         life_bar_bkgd = pygame.Surface(life_bar_rect.size)
-        life_bar_bkgd.fill((0, 0, 220))
+
+        if 150 < life <= 250:
+            life_bar_bkgd.fill(self.colors["blue"])
+        elif 50 <= life < 150:
+            life_bar_bkgd.fill(self.colors["yellow"])
+        elif 0 <= life < 50:
+            life_bar_bkgd.fill(self.colors["red"])
+        
         holder.dirtyrects.append(screen.blit(life_bar_bkgd, (70, 36)))
 
 
@@ -133,13 +161,14 @@ class StatsBar(object):
         """
         Generate a bottom border that contains the stats
         """
-
-        pygame.draw.line(
-            background, (0, 255, 0), (800, 70), (0, 70),
-        )
-        pygame.draw.line(background, (0, 255, 0), (320, 70), (320, 0))
+        # top line
         pygame.draw.line(background, (0, 255, 0), (800, 0), (0, 0))
-        pygame.draw.line(background, (0, 255, 0), (320, 35), (0, 35))
+        # bottom line.
+        pygame.draw.line(background, (0, 255, 0), (800, 70), (0, 70))
+        # divider line, divides bars from points, vertical line
+        pygame.draw.line(background, (0, 255, 0), (320, 70), (320, 0)) 
+        # middle horizontal line
+        pygame.draw.line(background, (0, 255, 0), (800, 35), (0, 35))
 
 
     def generate_virus_killed(self, screen, holder):
@@ -173,10 +202,12 @@ class StatsBar(object):
         Update everyting.
         """
 
+        self.stats_background(screen, holder)
         self.generate_unicorn_life_text(screen, holder)
-        self.generate_unicorn_life_bar(screen, holder)
+        self.generate_player_life_bar(screen, holder)
         self.generate_energy_bar_text(screen, holder)
         self.generate_virus_killed(screen, holder)
         self.generate_energy_bar(screen, holder)
         self.generate_game_time(screen, holder)
         self.generate_grid(background)
+        
